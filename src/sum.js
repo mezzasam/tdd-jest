6b48d11f62ca6e6d27496ea36c24baa37aa6b4c3
mezzasam <maeva.mezza38@gmail.com>
2020-09-17 10:43:18 +0200

    Tests sum & type

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,11 @@
 // Implement the sum function here
+
+function sum(n1, n2) {
+    if(typeof n1 === "number" && typeof n2 === "number") {
+        return n1 + n2;
+    } else {
+        return false;
+    }
+}
+
+module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,10 @@
 // Add the unit tests of the sum function here
+const sum = require('../src/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('type verification', () => {
+    expect(sum(1,'v9')).toBe(false);
+});
